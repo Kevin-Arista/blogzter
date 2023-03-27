c52082d3f6231c1a130849a56f8cb00d85ede822
kevin <kevin,a8988@gmail.com>
2023-03-27 19:40:53 -0400

    created form template

--- frontend/src/components/form-component.js
+++ frontend/src/components/form-component.js
@@ -2,8 +2,19 @@ import React from "react";
 
 function BlogForm(props){
     return(
-        <div>
-            This is a form!
+        <div className="BlogForm">
+            <form action="" method="post">
+            <label for="blog_title">Title:</label>
+            <input name="blog_title"type="text"/>
+            <br/>
+            <label for="blog_author">Author:</label>
+            <input name="blog_author"type="text"/>
+            <br/>
+            <label for="blog_content">Content:</label>
+            <input name="blog_content"type="text"/>
+            <br/>
+            <input type="submit" value="Submit" />
+            </form>    
         </div>
     );
 }
